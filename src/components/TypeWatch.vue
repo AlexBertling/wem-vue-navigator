d5a0dae729d141b577b6811a0f196ef79c6b213b
Alexander Bertling <alexander-bertling@web.de>
2021-12-08 17:27:58 +0100

    TypeWatch (A10.1)

---
+++ src/components/TypeWatch.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+      <div><label for="type">Please type: </label><input id="type" type="text" v-model="text"></div>
+      <div># Letters: {{text.split(" ").join("").length}}</div>
+      <div># Spaces: {{text.split(" ").length -1}}</div>
+      <div># Words: {{text.trim().split(/\s+/).length}}</div>
+    </div>
+    
+</template>
+
+<script>
+module.exports = {
+  name: 'TypeWatch',
+  data: function() {
+    return {
+      text: "test"
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
